2a7eab1eca7132989b6be84a0c6d38b3bdcfb878
Oscar D. Fortaleché <lechediaz@gmail.com>
2022-07-09 19:40:04 -0500

    Se crea ejemplo en una página web clásica

---
+++ classic-web/my-logic.js
@@ -0,0 +1,33 @@
+function crearCards() {
+  const info = [
+    {
+      title:
+        'Thor: Love and Thunder’ hace de los cortos de Darryl canon en el MCU',
+      category: 'Entretenimiento',
+    },
+    {
+      title: '3 consejos para evitar spoilers en internet',
+      category: 'Entretenimiento',
+    },
+    {
+      title:
+        'Google propone dividir su negocio publicitario ante señalamientos anticompetitivos',
+      category: 'Apps & Software',
+    },
+  ];
+
+  const containerRef = document.getElementsByClassName('container')[0];
+  const containerHtml = info.map(
+    (art) => `<article class="card">
+        <div class="card-left"></div>
+        <div class="card-right">
+          <h2>${art.category}</h2>
+          <p>${art.title}</p>
+        </div>
+      </article>`
+  );
+
+  containerRef.innerHTML = containerHtml;
+}
+
+crearCards();
