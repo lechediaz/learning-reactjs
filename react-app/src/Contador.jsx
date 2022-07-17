16f72c52b6ac93c1c02a0a867185f0c4381bcbf8
Oscar D. Fortaleché <lechediaz@gmail.com>
2022-07-16 21:38:55 -0500

    Se cambia código de renderizado condicional al componente *ContadorClase*

--- react-app/src/Contador.jsx
+++ react-app/src/Contador.jsx
@@ -7,24 +7,9 @@ function Contador() {
     setCantidadClicks(cantidadClicks + 1);
   };
 
-  const reiniciarContador = () => {
-    setCantidadClicks(0);
-  };
-
-  if (cantidadClicks === 10) {
-    return (
-      <div>
-        <p onClick={reiniciarContador}>
-          Amigo, por favor para. Haz otro click aquí para reiniciar.
-        </p>
-      </div>
-    );
-  }
-
   return (
     <div>
       <p>(Función) Total de Clicks: {cantidadClicks}</p>
-      {cantidadClicks === 9 && <p>Ya no aguanto un click más.</p>}
       <button onClick={aumentarCantidadClicks}>Presioname</button>
     </div>
   );
--- react-app/src/ContadorClase.jsx
+++ react-app/src/ContadorClase.jsx
@@ -15,10 +15,25 @@ export default class ContadorClase extends Component {
     }));
   };
 
+  reiniciarContador = () => {
+    this.setState({ cantidadClicks: 0 });
+  };
+
   render() {
+    if (this.state.cantidadClicks === 10) {
+      return (
+        <div>
+          <p onClick={this.reiniciarContador}>
+            Amigo, por favor para. Haz otro click aquí para reiniciar.
+          </p>
+        </div>
+      );
+    }
+
     return (
       <div>
         <p>(Clase) Total de Clicks: {this.state.cantidadClicks}</p>
+        {this.state.cantidadClicks === 9 && <p>Ya no aguanto un click más.</p>}
         <button onClick={this.aumentarClicks}>Presioname</button>
       </div>
     );
